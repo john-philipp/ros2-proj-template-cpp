8179a735e349b7e913115d68c952df8b509b208e
John-Philipp Drews <john-philipp.drews@gmx.de>
2024-06-19 10:51:28 +0200

    Actual template project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <iostream>
 
 int main()
 {
---
+++ src/node1.cpp
@@ -0,0 +1,9 @@
+#include <rclcpp/rclcpp.hpp>
+
+class Node1 : public rclcpp::Node {
+    public:
+        Node1() : Node("node1") {
+        }
+
+    
+};
